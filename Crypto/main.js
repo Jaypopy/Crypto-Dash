90830af7b54bb450eba3130416852955d74efb86
Jaypopy <Joh.hojer@gmail.com>
2021-05-07 16:24:04 +0200

    Added a lot of API Javascript stuff

--- Crypto/main.js
+++ Crypto/main.js
@@ -1,21 +1,86 @@
+let dashboardContent = document.querySelector(".dashboard-content");
+
 async function getCrypto()
     {
+        
         await fetch("https://api.coindesk.com/v1/bpi/currentprice.json", {
-
-        }  )
+            }  )
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                updateList(data);
+            }
+            )
+    
+        await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC&tsyms=USD,EUR", {
+        }
+        )
         .then(response => response.json())
         .then(data => console.log(data))
-        const myArticle = document.createElement('article');
-        const myH2 = document.createElement('h2');
-        const myPara1 = document.createElement('p');
-        const myPara2 = document.createElement('p');
-        const myPara3 = document.createElement('p');
-        const myList = document.createElement('ul');
-        myH2.textContent = bpi[0];
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myList);
+        .then(data => {
+            console.log(data)
+            updateList(data);
+        
+        }
+        )
+        await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD,EUR", {
+        }
+        )
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            updateList(data);
+        
+        }
+        )
+        await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=DOGE&tsyms=USD,EUR", {
+        }
+        )
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            updateList(data);
+        
+        }
+        )
+        await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=LTC&tsyms=USD,EUR", {
+        }
+        )
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            updateList(data);
+        
+        }
+        )
+        await fetch("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=zec&tsyms=USD,EUR", {
+        }
+        )
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            console.log(data)
+            updateList(data);
+        
+        }
+        )
     
+    
+
+    function updateList(data)
+    {
+        dashboardContent.innerHTML = "";
+        let cryptoName = document.createElement("h2");
+        let cryptoPrice = document.createElement("h2");
+        cryptoName.innerText = data.chartName;
+        cryptoPrice.innerText = data.bpi.USD.rate;
+        cryptoName.classList.add("crypto-name");
+        cryptoPrice.classList.add("crypto-price");
+        dashboardContent.append(cryptoName);
+        dashboardContent.append(cryptoPrice);
+
     }
+}
